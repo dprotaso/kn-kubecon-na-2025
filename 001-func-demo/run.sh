6b07b75ff9fb623bd50087eb9886634dec7bd165
Dave Protasowski <dprotaso@gmail.com>
2025-11-06 09:58:15 -0500

    first comment of func demo

---
+++ 001-func-demo/run.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+source $(git rev-parse --show-toplevel)/lib.sh
+
+# Cleanup old demo
+rm -rf square
+
+bx func create -l go square
+bx tree -C square
+bx bat square/handle.go
+
+cat <<EOF | patch square/handle.go
+--- square/handle.go	2025-11-05 23:35:13
++++ square/handle.new	2025-11-05 23:37:40
+@@ -1,26 +1,32 @@
+ package function
+ 
+ import (
+-	"fmt"
++	"encoding/json"
+ 	"net/http"
+-	"net/http/httputil"
++	"strconv"
+ )
+ 
++type Input struct {
++	Value string \`json:"value"\`
++}
++
++type Output struct {
++	Value string \`json:"value"\`
++}
++
+ // Handle an HTTP Request.
++// nolint
+ func Handle(w http.ResponseWriter, r *http.Request) {
+-	/*
+-	 * YOUR CODE HERE
+-	 *
+-	 * Try running \`go test\`.  Add more test as you code in \`handle_test.go\`.
+-	 */
++	var (
++		in  *Input
++		out Output
+ 
+-	dump, err := httputil.DumpRequest(r, true)
+-	if err != nil {
+-		http.Error(w, err.Error(), http.StatusInternalServerError)
+-		return
+-	}
++		dec = json.NewDecoder(r.Body)
++		enc = json.NewEncoder(w)
++	)
+ 
+-	fmt.Println("Received request")
+-	fmt.Printf("%q\n", dump)
+-	fmt.Fprintf(w, "%q", dump)
++	dec.Decode(&in)
++	val, _ := strconv.Atoi(in.Value)
++	out.Value = strconv.Itoa(val*val)
++	enc.Encode(out)
+ }
++
+EOF
+
+bx bat square/handle.go
+
+pushd square >/dev/null
+  bx func build --registry dprotaso
+  x func run &
+  read
+  bx func invoke --data '{"value":"9"}'
+popd >/dev/null
+
+kill %1
+wait
---
+++ lib.sh
@@ -0,0 +1,16 @@
+bx() {
+  local key
+
+  while true; do
+    x "$@"
+    read -n1 -s -r key
+    [[ $key == r ]] && continue
+    [[ $key == q ]] && exit
+    break
+  done
+}
+
+x() {
+  echo ">> $@"
+  "$@"
+}
