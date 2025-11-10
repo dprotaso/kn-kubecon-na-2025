d1e188c13f6d2dad36f9f060fd9dc99a8da2af6a
Dave Protasowski <dprotaso@gmail.com>
2025-11-10 17:17:20 -0500

    update run script

--- 003-eventing-demo/run.sh
+++ 003-eventing-demo/run.sh
@@ -4,6 +4,8 @@ source $(git rev-parse --show-toplevel)/lib.sh
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
-# bx kubectl apply -f manifest.yaml
-# bx bat data.json
-# bx curl -k https://svc-intake.keventmesh.margarita.dev -H 'Content-Type: application/json" -d @data.json
+bx kubectl apply -f manifest.yaml
+x kubectl wait --for=condition=Ready ksvc --all --timeout 5m
+bx kubectl get ksvc
+open -na "Google Chrome" --args --new-window https://intake.default.margarita.dev 
+open -na "Google Chrome" --args --new-window https://sockeye.default.margarita.dev
